53a75a3a87ee6290c25e4f4e57fcd04ace927a6e
MartinLavanant <martin.l@apps-panel.com>
2015-09-23 19:11:04 +0200

    retouche

--- static/js/chat.js
+++ static/js/chat.js
@@ -577,10 +577,6 @@
       })
 
       .on('joined_room', function() {
-          $(room_icon).fadeOut(function() {
-              room_icon.setAttribute('class', 'fa fa-users');
-              $(room_icon).fadeIn();
-          });
           $(room_name).html(rtc.room);
       })
 
